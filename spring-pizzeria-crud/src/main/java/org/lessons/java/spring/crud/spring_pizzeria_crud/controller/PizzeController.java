57321894a20b77bdd039cea71557cbad0ef1d527
AleB-135 <alessiobrusca1999@outlook.com>
2025-06-22 19:15:53 +0200

    feat: Aggiunta pagina per singola pizza + barra di ricerca

--- spring-pizzeria-crud/src/main/java/org/lessons/java/spring/crud/spring_pizzeria_crud/controller/PizzeController.java
+++ spring-pizzeria-crud/src/main/java/org/lessons/java/spring/crud/spring_pizzeria_crud/controller/PizzeController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -19,11 +21,24 @@ public class PizzeController {
     private PizzaRepository pizzaRepository;
 
     @GetMapping
-    public String index(Model model){
-
-        List<Pizza> Pizza = pizzaRepository.findAll();  //Prenderà dal DB i dati che servono
+    public String index(Model model, @RequestParam(name = "keyword", required = false) String keyword){ 
+        //Mi aspetto di ricevere un parametro chiamato "keyword". Ciò che è salvato dentro la chiave keyword dev'essere salvato d'ora in poi con la stringa "keyword".
+        List<Pizza> Pizza;
+        if (keyword != null && !keyword.isEmpty()) {
+            Pizza = pizzaRepository.findByNameContainingIgnoreCase(keyword);
+            //Se la keyword non è vuota e sto cercando qualcosa nello specifico, torna in repository e chiama il metodo sopracitato "(findByNameContainingIgnoreCase(keyword))" riportando solo i nomi delle pizze che abbiano al loro interno anche solo una parte del nome che possiedono.
+        } else {
+            Pizza = pizzaRepository.findAll();
+            //Se la keyword è vuota oppure è null, allora prendi una index normale
+        }
         model.addAttribute("pizze", Pizza);
         return"pizze/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("Pizza", pizzaRepository.findById(id).get());
+        return "pizze/show";
     }
 
 }
